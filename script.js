f3054acf16d8e6a915499deb1a8552729c4d68b8
Ergyon <benoitdoudard56@gmail.com>
2025-05-03 06:08:52 +0200

    chose starter, pokemon attack menu, animations,

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ resetBtn.addEventListener('click', () => {
 
 
 
-// Pokemons
+// Chose a Pokemon starter
 const pokeball_1 = document.getElementById('pokeball-1')
 const pokeball_2 = document.getElementById('pokeball-2')
 const pokeball_3 = document.getElementById('pokeball-3')
@@ -35,7 +35,6 @@ function revealPokemon(pokeball, imagePath) {
         pokeball.setAttribute('src', imagePath)
         pokeball.classList.add('pokemon')
         pokeball.classList.remove('pokeball')
-        void pokeball.offsetWidth;
 
         const fileName = imagePath.split('/').pop()
         const imageName = fileName.split('.')[0]
@@ -66,3 +65,60 @@ revealPokemon(pokeball_1, 'img/hericendre.png')
 revealPokemon(pokeball_2, 'img/kaiminus.png')
 revealPokemon(pokeball_3, 'img/germignon.png')
 
+
+// Pokemons battle
+const pokemons = document.querySelectorAll('.pkmnBattle')
+const menuAttack = document.querySelectorAll('.attack-menu')
+
+
+// Show only the selected pokemon's menu
+pokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        document.querySelectorAll(".attack-menu").forEach(menu => {
+            menu.style.display = 'none'
+        })
+        
+        const menu = pokemon.parentElement.querySelector('.attack-menu')
+        
+        if (menu) {
+            menu.style.display = 'block'
+        }
+        
+        document.addEventListener('click', (event) => {
+            if (!menu.contains(event.target) && event.target !== pokemon) {
+                menu.style.display = 'none'
+            }
+        })
+    })
+})
+
+// Color cells based on the type
+const typeColors = {
+    Feu: '#f1570fde',     
+    Eau: '#3b9cf1',     
+    Normal: '#e7eff0d7',  
+    Plante: '#7AC74C',
+}
+
+document.querySelectorAll('.attack-menu tbody tr').forEach(row => {
+    const attackCell = row.querySelector('td.attack')
+    const typeCell = attackCell.nextElementSibling
+    const typeTxt = typeCell.textContent.trim()
+
+    if (typeColors[typeTxt]) {
+        const color = typeColors[typeTxt]
+        typeCell.style.background = color
+    }
+})
+
+
+function selectAttack(attackName) {
+    const attackTxtWrapper = document.querySelector('.attack-txt-container')
+    attackTxtWrapper.innerHTML = ''
+    
+    const attackTxt = document.createElement('span')
+    attackTxt.classList.add('attack-txt')
+    attackTxt.innerText = "Vous lancez l'attaque " + attackName
+    attackTxtWrapper.appendChild(attackTxt)
+    
+}
